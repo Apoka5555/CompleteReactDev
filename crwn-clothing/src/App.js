6d16505e3d268f4e09c6e8ac11a1249834b16dce
Apoka5555 <apoka5555@gmail.com>
2021-11-20 23:06:02 +0600

    components

--- crwn-clothing/src/App.js
+++ crwn-clothing/src/App.js
@@ -1,8 +1,17 @@
-import logo from "./logo.svg";
-import "./App.css";
+import React from 'react';
 
-function App() {
-  return <div>Hello World</div>;
+import HomePage from './homepage.component';
+
+import './App.css';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className='App'>
+        <HomePage />
+      </div>
+    );
+  }
 }
 
 export default App;
